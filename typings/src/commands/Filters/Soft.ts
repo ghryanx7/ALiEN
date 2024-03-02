07c853090f1984f5908d26910a9b436f9c013805
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:57:01 +0600

    Update Soft.ts

--- typings/src/commands/Filters/Soft.ts
+++ typings/src/commands/Filters/Soft.ts
@@ -1,6 +1,6 @@
-import AvonCommand from "../../base/AvonCommand.js";
+import ALiENCommand from "../../base/ALiENCommand.js";
 
-export default class Soft extends AvonCommand {
+export default class Soft extends ALiENCommand {
   constructor(client: any) {
     super(client);
     this.name = "soft";
