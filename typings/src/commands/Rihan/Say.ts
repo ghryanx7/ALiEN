5a026e27fe6ceb2a7f1eb61bc201a29c92a3852f
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:47:41 +0600

    Update Say.ts

--- typings/src/commands/Rihan/Say.ts
+++ typings/src/commands/Rihan/Say.ts
@@ -1,6 +1,6 @@
-import AvonCommand from "../../base/AvonCommand.js";
+import ALiENCommand from "../../base/ALiENCommand.js";
 
-export default class Say extends AvonCommand {
+export default class Say extends ALiENCommand {
   constructor(client: any) {
     super(client);
     this.name = "say";
