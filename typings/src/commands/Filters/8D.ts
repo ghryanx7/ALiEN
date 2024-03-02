72ebfa3aec5d9223fba2d4e0fd4c52bb75eff13e
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:49:04 +0600

    Update 8D.ts

--- typings/src/commands/Filters/8D.ts
+++ typings/src/commands/Filters/8D.ts
@@ -1,6 +1,6 @@
-import AvonCommand from "../../base/AvonCommand.js";
+import ALiENCommand from "../../base/ALiENCommand.js";
 
-export default class EightD extends AvonCommand {
+export default class EightD extends ALiENCommand {
   constructor(client: any) {
     super(client);
     this.name = "8d";
