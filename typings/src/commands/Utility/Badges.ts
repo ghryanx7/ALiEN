c0c8cee5e4d0ec94b88d3b89460dc239dc78dab8
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:37:33 +0600

    Update Badges.ts

--- typings/src/commands/Utility/Badges.ts
+++ typings/src/commands/Utility/Badges.ts
@@ -13,17 +13,17 @@ export default class Badges extends Object {
   communityManager: string;
   constructor() {
     super();
-    this.owner = "1129669328371990579";
-    this.developer = "1129667888127680592";
+    this.owner = "1085376019445321829";
+    this.developer = "1085376019445321829";
     this.communityManager = "";
     this.staff = "1065489475028930642";
-    this.admin = "1065488468999929936";
-    this.codev = "1129668221574844497";
-    this.supporter = "1071291114411270217";
-    this.vip = "1129668638866153482";
+    this.admin = "1085376019445321829";
+    this.codev = "1085376019445321829";
+    this.supporter = "1085376019445321829";
+    this.vip = "1085376019445321829";
     this.friend = "";
-    this.user = "1053336995608403979";
-    this.bug = "1129667685601513562";
-    this.mod = "1107839131473678416";
+    this.user = "1085376019445321829";
+    this.bug = "1085376019445321829";
+    this.mod = "1085376019445321829";
   }
 }
