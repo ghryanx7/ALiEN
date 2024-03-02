c11fe052c5c3213103c79113bfbd0e41bf14985e
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:31:26 +0600

    Update Vote.ts

--- typings/src/commands/Information/Vote.ts
+++ typings/src/commands/Information/Vote.ts
@@ -1,6 +1,6 @@
-import AvonCommand from "../../base/AvonCommand.js";
+import ALiENCommand from "../../base/ALiENCommand.js";
 
-export default class Vote extends AvonCommand {
+export default class Vote extends ALiENCommand {
   constructor(client: any) {
     super(client);
     this.name = "vote";
