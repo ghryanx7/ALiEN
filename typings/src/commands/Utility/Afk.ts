613a7a484f8ee7f885fdfd6a8086cb23503e8100
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:33:38 +0600

    Update Afk.ts

--- typings/src/commands/Utility/Afk.ts
+++ typings/src/commands/Utility/Afk.ts
@@ -1,7 +1,7 @@
 import { addGlobalAfk, addServerAfk } from "../../api/db/premium.js";
-import AvonCommand from "../../base/AvonCommand.js";
+import ALiENCommand from "../../base/ALiENCommand.js";
 
-export default class Afk extends AvonCommand {
+export default class Afk extends ALiENCommand {
   constructor(client: any) {
     super(client);
     this.name = "afk";
