0c1f734809e3bc5380b74a609c7f52c7210b852d
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:57:38 +0600

    Update Treblebass.ts

--- typings/src/commands/Filters/Treblebass.ts
+++ typings/src/commands/Filters/Treblebass.ts
@@ -1,6 +1,6 @@
-import AvonCommand from "../../base/AvonCommand.js";
+import ALiENCommand from "../../base/ALiENCommand.js";
 
-export default class Treblebass extends AvonCommand {
+export default class Treblebass extends ALiENCommand {
   constructor(client: any) {
     super(client);
     this.name = "treblebass";
