c0201b8536721425f9f736c951cfdfc0bfd82808
RY4N <142653742+ghryanx7@users.noreply.github.com>
2024-03-02 17:30:34 +0600

    Update Support.ts

--- typings/src/commands/Information/Support.ts
+++ typings/src/commands/Information/Support.ts
@@ -1,6 +1,6 @@
-import AvonCommand from "../../base/AvonCommand.js";
+import ALiENCommand from "../../base/ALiENCommand.js";
 
-export default class Support extends AvonCommand {
+export default class Support extends ALiENCommand {
   constructor(client: any) {
     super(client);
     this.name = "support";
